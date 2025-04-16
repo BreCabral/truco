9f5c92e85e23462d1972b6445a7c5bde30995928
Breno <breno.ara.cabral@gmail.com>
2025-04-16 18:14:46 -0300

    build: start SpringBoot API

---
+++ backend/api-truco/src/test/java/com/batetruco/api_truco/TrucoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batetruco.api_truco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrucoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
